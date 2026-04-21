4fc7963c952418c1f067d8f0833108116f47e2be
a-rossman0825 <anthony.rossman0825@gmail.com>
2026-04-21 16:05:45 -0600

    chore: define homepage layout with section component creation

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <section class="bg-light text-dark h-20 w-full">
+    <div class="flex items-center justify-between w-full bg-light text-dark tracking-[0.2em] px-2 py-2">
+      <a href="#">Site Map</a>
+      <a href="#">FAQ</a>
+      <a href="#">Facebook</a>
+      <a href="#">Instagram</a>
+      <a href="#">Yahoo.answers</a>
+      <a href="#">TruthSocial</a>
+      <a href="#">Joe's Personal Email</a>
+    </div>
+    <div class="w-full h-20 bg-dark"></div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import Footer from '~/components/Footer.vue';
 import Hero from '~/components/Hero.vue';
 import Media from '~/components/Media.vue';
+import Merch from '~/components/Merch.vue';
 import Music from '~/components/Music.vue';
+import Shows from '~/components/Shows.vue';
 
 </script>
 
@@ -9,4 +12,7 @@ import Music from '~/components/Music.vue';
   <Hero />
   <Media />
   <Music />
+  <Merch />
+  <Shows />
+  <Footer />
 </template>
