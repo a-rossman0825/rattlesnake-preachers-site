8ac74be9131913aa1e9b96a17ab77052c200dba6
a-rossman0825 <anthony.rossman0825@gmail.com>
2026-04-21 18:58:25 -0600

    feat: Add Nav links direct to necessary pages, Add necessary pages

---
+++ src/pages/contact.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <section class="bg-dark text-light text-center justify-center items-center flex h-screen w-full">
+    <h1>This is the Contact Page</h1>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
