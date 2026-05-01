b243bc5e6246dad86d57a143b58ea08d6913b8de
a-rossman0825 <anthony.rossman0825@gmail.com>
2026-04-30 21:52:55 -0600

    feat: struct page scroll animations

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,21 +5,31 @@ import Media from '~/components/sections/Media.vue'
 import Merch from '~/components/sections/Merch.vue'
 import Music from '~/components/sections/Music.vue'
 import Shows from '~/components/sections/Shows.vue'
+import { usePageScroll } from '~/composables/usePageScroll'
+
+// Init scroll anims
+usePageScroll()
 </script>
 
 <template>
-  <Hero />
-  <Media />
-  <div class="music-section-wrapper w-full">
-    <Music />
+  <div class="page-wrapper">
+    <Hero class="hero-section page-section" />
+    <Media class="media-section page-section" />
+    <div class="music-section-wrapper w-full">
+      <Music class="music-section page-section" />
+    </div>
+    <Merch class="merch-section page-section" />
+    <Shows class="shows-section page-section" />
+    <Footer />
   </div>
-  <Merch />
-  <Shows />
-  <Footer />
 </template>
 
 <style lang="scss" scoped>
 .music-section-wrapper {
   height: 95dvh;
+}
+
+.page-wrapper {
+  position: relative;
 }
 </style>
